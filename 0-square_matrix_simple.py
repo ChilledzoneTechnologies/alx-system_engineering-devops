5037ea8f5d75baa256213c403c0592b5eef94d67
ChilledzoneTechnologies <narteytettehe@gmail.com>
2024-02-03 07:53:56 -0800

    0-change_your_home_IP

--- 0-square_matrix_simple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    newMatrix = matrix.copy()
-    for i in range(len(matrix)):
-        newMatrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return newMatrix
--- 1-search_replace.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def search_replace(my_list, search, replace):
-    newList = my_list.copy()
-    newList = list(map(lambda x: replace if x == search else x, newList))
-    return newList
